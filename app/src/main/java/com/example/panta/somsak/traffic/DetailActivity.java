466b9ed861e3d817703b0d29d3480fed4f9e8c5d
somsakPanta <soms2819@gmail.com>
2015-10-04 14:36:25 +0700

    Detail activity success

--- app/src/main/java/com/example/panta/somsak/traffic/DetailActivity.java
+++ app/src/main/java/com/example/panta/somsak/traffic/DetailActivity.java
@@ -2,12 +2,59 @@ package com.example.panta.somsak.traffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    //Explicit
+    TextView titleTextView, detailTextView;
+    ImageView trafficImageView;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        //Bind Widget
+        bindWiget();  //Alt+Enter Shot key to create method
+
+        //Show View
+        showView();
+
+    } //On Create
+
+    public void clickBack(View view) {
+        //  จะต้องไปเปิด android:onClick="clickBack" ที่หน้า activity_detail.xml
+        //finish คือการปิด Activity นั้น ๆ
+        finish();
+    }// Click back
+
+
+
+    private void showView() {
+        //Show title
+        String strTitle = getIntent().getStringExtra("Title");
+        titleTextView.setText(strTitle);
+
+        //Show Image
+        int intIcon = getIntent().getIntExtra("Image", R.drawable.traffic_01);
+        trafficImageView.setImageResource(intIcon);
+
+        //Show Detail อ้างไปถึง File Xml ที่ได้สร้างไว้ที่ โฟร์เดอร์ Value ชื่อ Detail.xml
+        String[] strDetail = getResources().getStringArray(R.array.detail);
+
+        detailTextView.setText(strDetail[getIntent().getIntExtra("index",0)]);
     }
-}
+
+    private void bindWiget() {
+        titleTextView = (TextView) findViewById(R.id.txtDetailTitle);
+        detailTextView = (TextView) findViewById(R.id.txtDetail);
+        trafficImageView = (ImageView) findViewById(R.id.imvDetailTraffic);
+
+    }
+} //Main class
--- app/src/main/java/com/example/panta/somsak/traffic/MainActivity.java
+++ app/src/main/java/com/example/panta/somsak/traffic/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void createListView() {
     //กำหนด String of picture เพื่ออธิบายภาพ
-        String[] strTitle = new String[20];
+        final String[] strTitle = new String[20];
         strTitle[0] = "ห้ามเลี้ยวซ้าย";
         strTitle[1] = "ห้ามเลี้ยวขวา";
         strTitle[2] = "ตรงไป";
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         strTitle[19] = "จำกัดความสูง 5 เมตร";
 
         //Source of picture
-        int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02, R.drawable.traffic_03,
+        final int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02, R.drawable.traffic_03,
                 R.drawable.traffic_04, R.drawable.traffic_05, R.drawable.traffic_06,
                 R.drawable.traffic_07, R.drawable.traffic_08, R.drawable.traffic_09,
                 R.drawable.traffic_10, R.drawable.traffic_11, R.drawable.traffic_12,
@@ -75,6 +75,15 @@ public class MainActivity extends AppCompatActivity {
         trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                //Intent to DetailActivity
+
+                Intent objIntent = new Intent(MainActivity.this,DetailActivity.class);
+                objIntent.putExtra("Title",strTitle[position]);
+                objIntent.putExtra("Image", intIcon[position]);
+                objIntent.putExtra("index", position);
+                startActivity(objIntent);
 
             } //Even on Item click
         });
